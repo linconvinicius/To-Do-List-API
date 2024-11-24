69a429f7b1011d999834baaf528fb7921d8df75e
linconvinicius <lvini_@live.com>
2024-11-24 17:57:15 -0300

    add lombok in project

--- src/main/java/br/com/todolist/todolist/user/UserModel.java
+++ src/main/java/br/com/todolist/todolist/user/UserModel.java
@@ -1,7 +1,12 @@
 package br.com.todolist.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
+
+
 }
